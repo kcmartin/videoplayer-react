479fec70df712e648a9f456af55df5ce2ace7b53
kcmartin <kcmartin@users.noreply.github.com>
2016-07-05 17:00:14 -0700

    Test yt search
    Console logging returned search data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
-import { youtubeKey } from './keys';
+import { API_KEY } from './keys';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 //   Create a new component.
 //   This component should produce some HTML
